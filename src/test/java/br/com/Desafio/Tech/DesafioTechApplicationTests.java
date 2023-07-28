04286703d176ff6468dd458d6e8be74b1b3e8855
marcosalexandre100 <marcosalexandre100@gmail.com>
2023-07-28 20:09:26 -0300

    Projeto inicial

---
+++ src/test/java/br/com/Desafio/Tech/DesafioTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Desafio.Tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
